d0823e85074dc7b4128cc914e0c32cc8977cf18a
virt19804 <virt19804@sn-69-2.tll07.zoneas.eu>
2017-11-05 14:20:54 +0200

    Halfway with CSS circles

---
+++ app.js
@@ -0,0 +1,30 @@
+function getData() {
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myObj = JSON.parse(this.responseText);
+            document.getElementById("last_updated").innerHTML = myObj.last_updated;
+            document.getElementById("curr_temp").innerHTML = myObj.curr_temp;
+            document.getElementById("curr_hum").innerHTML = myObj.curr_hum;
+            document.getElementById("curr_soil_temp").innerHTML = myObj.curr_soil_temp;
+            document.getElementById("curr_soil_moisture").innerHTML = myObj.curr_soil_moisture;
+            document.getElementById("curr_water_temp").innerHTML = myObj.curr_water_temp;
+            document.getElementById("curr_temp_clean").innerHTML = myObj.curr_temp_clean;
+            document.getElementById("curr_hum_clean").innerHTML = myObj.curr_hum_clean;
+            document.getElementById("curr_soil_temp_clean").innerHTML = myObj.curr_soil_temp_clean;
+            document.getElementById("curr_soil_moisture_clean").innerHTML = myObj.curr_soil_moisture_clean;
+            document.getElementById("curr_water_temp_clean").innerHTML = myObj.curr_water_temp_clean; 
+        }
+    };
+    xmlhttp.open("GET", "server.php", true);
+    xmlhttp.send();
+    console.log("Updated ");
+    //Gets data from DB every minute
+    setTimeout(getData, 60000);
+
+}
+getData();
+
+
--- server.php
+++ server.php
@@ -4,10 +4,20 @@ include 'functions.php';
 
 // DEFINE ALL VALUES FROM DB
 $currTemp = round($currTempArray[0]['avg(temperature)'], 1) . '&deg;C';
+$currTempClean = round($currTempArray[0]['avg(temperature)'], 0);
+
 $currHum = round($currHumArray[0]['avg(humidity)'], 1) . '&#37;';
+$currHumClean = round($currHumArray[0]['avg(humidity)'], 0);
+
 $currWaterTemp = round($currTempWaterArray[0]['temp'], 1) . '&deg;C';
+$currWaterTempClean = round($currTempWaterArray[0]['temp'], 0);
+
 $currSoilMoisture = round($currSoilMoistureArray[0]['avg(moistureperc)'], 1) . '&#37;';
-$currSoilTemp = round($currSoilTempArray[0]['avg(temperature)'], 1) . '&#37;'; 
+$currSoilMoistureClean = round($currSoilMoistureArray[0]['avg(moistureperc)'], 0);
+
+$currSoilTemp = round($currSoilTempArray[0]['avg(temperature)'], 1) . '&deg;C';  
+$currSoilTempClean = round($currSoilTempArray[0]['avg(temperature)'], 0); 
+
 $lastUpdated = "Last updated: " . date('m/d/Y H:i:s', Time());
 
 
@@ -15,12 +25,17 @@ $lastUpdated = "Last updated: " . date('m/d/Y H:i:s', Time());
 
 $myObj = new stdClass();
 $myObj->name = "Greenhouse data";
-$myObj->last_updated = "$lastUpdated";
+$myObj->last_updated = "$lastUpdated"; 
 $myObj->curr_temp = "$currTemp";
+$myObj->curr_temp_clean = "$currTempClean";
 $myObj->curr_hum = "$currHum";
+$myObj->curr_hum_clean = "$currHumClean";
 $myObj->curr_soil_temp = "$currSoilTemp";
-$myObj->curr_soil_moisture = "$currSoilMoisture"; 
+$myObj->curr_soil_temp_clean = "$currSoilTempClean";
+$myObj->curr_soil_moisture = "$currSoilMoisture";
+$myObj->curr_soil_moisture_clean = "$currSoilMoistureClean"; 
 $myObj->curr_water_temp = "$currWaterTemp";
+$myObj->curr_water_temp_clean = "$currWaterTempClean";
 
 $currDataJSON = json_encode($myObj);
 
